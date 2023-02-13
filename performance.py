90e5df96b0baea54a25b3bf5fc215aaea2f8bc4f
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2023-02-13 21:43:24 +0200

    more comments

--- performance.py
+++ performance.py
@@ -2,26 +2,27 @@ import json
 import requests
 
 
-def run():   # See how clean my run() is? Always try to keep run() clean
+def run():
+    # See how clean my run() is? Always try to keep run() clean
+    # Also note how the order in run() reflects the order you find the methods() below? Easy to read, right? : )
     print_welcome()
-    performance_report = query_performance()    # see below on how to do 'return' values
+    performance_report = query_performance()    # see (*) on How To 'return' Values From Functions
     create_report(performance_report)
-
-# see how the order in run() reflects the order you find the methods() below? Easy to read : )
 
 
 def print_welcome():   # it's always a good idea to tell people how your application works
     print("Create your performance reports with ease.")
     print("Please enter the domain in a format like www.dental21.de")
-    # I skip asking for input, this is just an example on how to write well structured software : )
+    # I skip asking for any input, this is just an example on how to write well structured software : )
 
 
 def query_performance():
     web_response = requests.get(url)
-    return json.loads(web_response.content)  # with return you specify what should come out of the function
+    json_result = json.loads(web_response.content)  # (*) with 'return' you specify what should come out of the function
+    return json_result      # can you think of how to write the return statement with one line less?
 
 
-def create_report(report):                   # Parameters go into the method
+def create_report(report):
     print("lighthouse fetchTime")
     print(report['lighthouseResult']['fetchTime'])
     print("form factor")
@@ -32,7 +33,7 @@ def create_report(report):                   # Parameters go into the method
 
 
 if __name__ == '__main__':
-    # you can define global variables here, like I do with 'url'
+    # Maybe you noticed that this is just an 'if' and not actually a function()?
+    # Let's talk about 'scope' and why 'url' is behaving like a 'global variable'
     url = "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://dental21.de"
-    # see how url is not a parameter but I still use it in run()
     run()
