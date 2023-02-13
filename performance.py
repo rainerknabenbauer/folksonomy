635e1c4c0a94de6b9d036e395c449f53939e93b0
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2023-02-13 21:30:28 +0200

    improve comments

--- performance.py
+++ performance.py
@@ -2,6 +2,14 @@ import json
 import requests
 
 
+def run():   # See how clean my run() is? Always try to keep this one clean
+    print_welcome()
+    performance_report = query_performance()    # see below on how to do 'return' values
+    create_report(performance_report)
+
+# see how the order in run() reflects the order you find the methods() below? Easy to read : ) 
+
+
 def print_welcome():   # it's always a good idea to tell people how your application works
     print("Create your performance reports with ease.")
     print("Please enter the domain in a format like www.dental21.de")
@@ -10,7 +18,7 @@ def print_welcome():   # it's always a good idea to tell people how your applica
 
 def query_performance():
     web_response = requests.get(url)
-    return json.loads(web_response.content)  # return values go out of the method
+    return json.loads(web_response.content)  # with return you specify what should come out of the function
 
 
 def create_report(report):                   # Parameters go into the method
@@ -20,12 +28,7 @@ def create_report(report):                   # Parameters go into the method
     print(report['lighthouseResult']['configSettings']['formFactor'])
     print("overall score")
     print(report["lighthouseResult"]["categories"]["performance"]["score"] * 100)
-
-
-def run():   # See how clean my run() is? Always try to keep this one clean
-    print_welcome()
-    performance_report = query_performance()    # remember the 'return' value? performance_report now has the value!
-    create_report(performance_report)
+    # .. and so on - if you print it or directly add it to CSV is up to you
 
 
 if __name__ == '__main__':
